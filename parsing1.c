de10174283d22beba47467d70620f07d1bf9b4ff
Ian Mark <imark@royal-23.cs.wisc.edu>
2018-11-02 16:40:36 -0500

    incremental testing

--- parsing1.c
+++ parsing1.c
@@ -9,12 +9,11 @@
 *
 ***********************************************************************/
 
-#include "reader.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "queue.h"
-#include <pthread.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
  *  This method reads in lines from make and parse through them.
@@ -26,23 +25,28 @@ void read(FILE* q) {
 	const int BUF_SIZE = 1024;
 	int type = -1; //type of line, 0 if cmd 1 if target, 2 if blank
        // First, read through line and store as string
-       char input  = '';
-       char* line = "";
+       char input  = ' ';
+       char* line = calloc(sizeof(char), BUF_SIZE);
+       char* buffer = calloc(sizeof(char), BUF_SIZE);
        input = getc(q);
-       while( input != 'EOF' && input != '\n' ) {
-		strncat(line, input, BUF_SIZE);
+       int ctr = 0;
+       while( input != EOF && input != '\n' ) {
+		buffer[ctr] = input;
 		input = getc(q);
+		ctr++;
       }
+      strncpy(line, buffer, 100);
+      printf("%s\n",buffer);
       if (line[0] == '\t') {
 		type = 0; 
       }
-      else if ( isalpha(line[0]) || isdigit(line[0]) {
+      else if ( isalpha(line[0]) || isdigit(line[0])) {
 		type = 1;
       }
       else if ( line[0] == '\n' ) {
 		type = 2;
       }
-      else { printf("error"); }
+      else { printf("error\n"); }
 
-	printf("%i", type);
+	printf("%i\n", type);
 }
